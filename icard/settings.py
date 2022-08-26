28c36c234f89faa1d3c9b7e25df8b9ccaba98a39
Juanito Ortega <ortegaj83@gmail.com>
2022-08-25 23:34:57 -0400

    app de usuarios reformada y mejorada

--- icard/settings.py
+++ icard/settings.py
@@ -82,15 +82,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'icard.wsgi.application'
 
+# USERS
+AUTH_USER_MODEL = 'users.User'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'icard_django',
+        'USER': 'jortega',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -144,7 +153,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Tabla personalizada para gestionar los usuarios
 # https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#using-a-custom-user-model
-AUTH_USER_MODEL = 'users.User'
+
 
 # Allow all host headers
 CORS_ORIGIN_ALLOW_ALL = True
--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 from users.models import User
 
 # Registramos nuestro modelo de usuario en el admin
-@admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    pass
+# Esta clase se crea después de crear, ejecutar las migraciones y después de crear el superuser
+class UserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
+    list_display_links = ('email',)
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, UserAdmin)
